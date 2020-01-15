3519ce5f6b8469a6c5880df5cf33c1b8eb1011c5
Aaron Taylor <aarontaylor95@hotmail.co.uk>
2020-01-15 20:48:16 +0000

    feat: checkout works with special offers

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Checkout
 {
@@ -22,7 +23,27 @@ namespace Checkout
         {
             var items = _itemScanner.List();
             var qualifiedOffers = _matchOffers.Match(items);
-            return items.Select(x => x.Key.UnitPrice * x.Value).Sum();            
+            var total = CalculateTotalAfterOffers(items, qualifiedOffers);
+
+            return total;
+        }
+        private decimal CalculateTotalAfterOffers(Dictionary<Item, int> items, Dictionary<SpecialOffer, int> offers)
+        {
+            var subTotal = 0m;
+            foreach(var item in items)
+            {
+                // refactor: look at better way that > 0 given time
+                var offer = offers.FirstOrDefault(x => x.Key.Sku == item.Key.Sku);
+                var itemsAfterOffer = 0;
+                if (offer.Value > 0)
+                {
+                    var offerTotal = offer.Value * offer.Key.OfferPrice;
+                    itemsAfterOffer = item.Value - (offer.Key.Quantity * offer.Value);
+                    subTotal += offerTotal;
+                }
+                subTotal += itemsAfterOffer * item.Key.UnitPrice;
+            }
+            return subTotal;
         }
     }
 }
--- CheckoutTests/CheckoutTests.cs
+++ CheckoutTests/CheckoutTests.cs
@@ -9,13 +9,17 @@ namespace CheckoutTests
     public class CheckoutTests
     {
         private Mock<IScanner> _scannerMock;
+        private Mock<IMatchOffers> _matchOfferMock;
         private Checkout.Checkout _checkout;
 
         [SetUp]
         public void Setup()
         {
             _scannerMock = new Mock<IScanner>();
-            _checkout = new Checkout.Checkout(_scannerMock.Object);
+            _matchOfferMock = new Mock<IMatchOffers>();
+            _checkout = new Checkout.Checkout(_scannerMock.Object, _matchOfferMock.Object);
+
+            _matchOfferMock.Setup(x => x.Match(It.IsAny<Dictionary<Item, int>>())).Returns(new Dictionary<SpecialOffer, int>());
         }
 
         [Test]
@@ -67,6 +71,46 @@ namespace CheckoutTests
             _scannerMock.Setup(x => x.List()).Returns(items);
             var expected = (TestItems.Apple.UnitPrice * itemCount) 
                 + (TestItems.Biscuit.UnitPrice * itemCount);
+
+            var result = _checkout.GetTotal();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldGetCorrectTotalWhenSingleSpecialOfferApplied()
+        {
+            var offers = new Dictionary<SpecialOffer, int>();
+            offers.Add(new SpecialOffer("A99", 3, 1.30m), 1);
+
+            var items = new Dictionary<Item, int>();
+            items.Add(TestItems.Apple, 4);
+
+            _scannerMock.Setup(x => x.List()).Returns(items);
+            _matchOfferMock.Setup(x => x.Match(items)).Returns(offers);
+            var expected = 1.3m + ( TestItems.Apple.UnitPrice * 1);
+
+            var result = _checkout.GetTotal();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ShouldGetCorrectTotalWhenMultipleOffersApplied()
+        {
+            var offers = new Dictionary<SpecialOffer, int>();
+            offers.Add(new SpecialOffer("A99", 3, 1.30m), 2);
+            offers.Add(new SpecialOffer("B15", 2, 0.45m), 2);
+
+            var items = new Dictionary<Item, int>();
+            items.Add(TestItems.Apple, 7);
+            items.Add(TestItems.Biscuit, 5);
+
+            _scannerMock.Setup(x => x.List()).Returns(items);
+            _matchOfferMock.Setup(x => x.Match(items)).Returns(offers);
+            // refactor: move special and hard coded prices into const 
+            var expected = 2.6m + (TestItems.Apple.UnitPrice * 1) +
+                           0.9m + (TestItems.Biscuit.UnitPrice * 1);
 
             var result = _checkout.GetTotal();
 
